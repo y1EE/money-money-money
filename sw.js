c376f4839d987f3e1734a32c206d11e46d845e6d
y1EE <115156841+y1EE@users.noreply.github.com>
2026-09-23 14:29:00 +0800

    v2.5
    1. 修改螢光標記顏色順序
    2. 簡化期間帳本列表

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yees-money-books-v7-20260923";
+const CACHE_NAME = "yees-money-books-v9-20260923";
 self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("activate",event=>{
   event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
